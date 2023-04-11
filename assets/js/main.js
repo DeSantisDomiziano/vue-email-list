d0db7a7047ede9f8a65df7192ec3e428dd279037
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-11 15:45:32 +0200

    fix bug same email

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,12 @@ createApp({
       }
     },
     mounted() {
-        axios
-        .get(this.url)
-        .then(resp => {
-            console.log(resp.data.response);
-            for(i=0; i < 10; i++) {
+        for(i = 0; i < 10; i++) {
+            axios
+            .get(this.url)
+            .then(resp => {
                 this.random_mail.push(resp.data.response)
-            }
-        })
+            })
+        }
     }
 }).mount('#app')
