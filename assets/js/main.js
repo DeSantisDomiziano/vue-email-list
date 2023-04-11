59e83c4622d728ccc96fae029bf0773d383b5a88
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-11 15:32:34 +0200

    take all object from API

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        
+        random_mail: null,
+        url: 'https://flynn.boolean.careers/exercises/api/random/mail'
       }
     },
     mounted() {
-
+        axios
+        .get(this.url)
+        .then(resp => {
+            console.log(resp);
+        })
     }
 }).mount('#app')
