857a874972b35e7321e306ee8320f5742b5ae3ad
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-12 09:47:21 +0200

    cleaner code

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,18 @@ createApp({
         url: 'https://flynn.boolean.careers/exercises/api/random/mail',
       }
     },
-    mounted() {
-        for(i = 0; i < 10; i++) {
-            axios
+    methods: {
+      generate_email(n_email) {
+        for(i = 0; i < n_email; i++) {
+          axios
             .get(this.url)
             .then(resp => {
                 this.random_mail.push(resp.data.response)
             })
         }
+      }
+    },
+    mounted() {
+      this.generate_email(10)
     }
 }).mount('#app')
